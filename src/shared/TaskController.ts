6e5401007a04a2250ee37857236c95e30787bbf5
Dev Agrawal <dev@clerk.dev>
2024-01-09 16:46:06 -0500

    controller method protected

--- src/shared/TaskController.ts
+++ src/shared/TaskController.ts
@@ -1,12 +1,14 @@
-import { BackendMethod, remult } from "remult"
+import { Allow, BackendMethod, remult } from "remult"
 import { Task } from "./Task"
 
 export class TasksController {
-  @BackendMethod({ allowed: true })
+  @BackendMethod({ allowed: Allow.authenticated })
   static async setAllCompleted(completed: boolean) {
     const taskRepo = remult.repo(Task)
 
-    for (const task of await taskRepo.find()) {
+    for (const task of await taskRepo.find({
+      where: { userId: remult.user!.id }
+    })) {
       await taskRepo.save({ ...task, completed })
     }
   }
