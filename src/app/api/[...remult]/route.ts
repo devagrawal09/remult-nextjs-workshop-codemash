52cf447d646b98f720d488a16042e8818bdf741c
Dev Agrawal <dev@clerk.dev>
2024-01-09 15:39:30 -0500

    working todo app

--- src/app/api/[...remult]/route.ts
+++ src/app/api/[...remult]/route.ts
@@ -1,26 +1,28 @@
 import { remultNextApp } from "remult/remult-next"
 import { Task } from "../../../shared/Task"
 import { TasksController } from "../../../shared/TaskController"
-import { getUserOnServer } from "../auth/[...nextauth]/route"
 import { createPostgresDataProvider } from "remult/postgres"
-import ably from "ably/promises"
+import { Rest } from "ably/promises"
 import { AblySubscriptionServer } from "remult/ably"
 import { DataProviderLiveQueryStorage } from "remult/server"
+import { auth } from "@clerk/nextjs"
+
 const dataProvider = createPostgresDataProvider({
-  connectionString: process.env["POSTGRES_URL"] || process.env["DATABASE_URL"],
-  configuration: {
-    ssl: Boolean(process.env["POSTGRES_URL"]),
-  },
+  connectionString: process.env["DATABASE_URL"]
 })
+
 const api = remultNextApp({
   entities: [Task],
   controllers: [TasksController],
-  getUser: getUserOnServer,
+  getUser: async () => {
+    const { userId } = auth()
+    if (userId) return { id: userId }
+  },
   subscriptionServer: new AblySubscriptionServer(
-    new ably.Rest(process.env["ABLY_API_KEY"]!)
+    new Rest(process.env["ABLY_API_KEY"]!)
   ),
   dataProvider,
-  liveQueryStorage: new DataProviderLiveQueryStorage(dataProvider),
+  liveQueryStorage: new DataProviderLiveQueryStorage(dataProvider)
 })
 
 export const { POST, PUT, DELETE, GET } = api
